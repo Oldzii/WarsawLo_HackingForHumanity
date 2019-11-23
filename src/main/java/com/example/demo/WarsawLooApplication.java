3f5d83b6a1c76ac1f792636a896afc6da2856393
Oldziii <olga23284@gmail.com>
2019-11-23 15:21:18 +0100

    jnijnijm`

---
+++ src/main/java/com/example/demo/WarsawLooApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarsawLooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarsawLooApplication.class, args);
+	}
+
+}
